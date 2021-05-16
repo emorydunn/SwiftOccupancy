50ab973184a011f8c1f734f3f32fc4eede6317ee
Emory Dunn <edunn@emorydunn.com>
2021-05-16 13:54:52 -0700

    Initial Commit

---
+++ Shared/ThermalViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThermalViewerApp.swift
+//  Shared
+//
+//  Created by Emory Dunn on 5/16/21.
+//
+
+import SwiftUI
+
+@main
+struct ThermalViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
