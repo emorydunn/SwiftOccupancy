6ca0856bfc75d850ff4e673efe3ab978e176736e
Emory Dunn <edunn@emorydunn.com>
2021-05-20 12:38:36 -0700

    Add test data

--- ThermalViewerTests/ThermalViewerTests.swift
+++ ThermalViewerTests/ThermalViewerTests.swift
@@ -6,11 +6,78 @@
 //
 
 import XCTest
+import Combine
 @testable import ThermalViewer
 
 class ClusterTests: XCTestCase {
     
     let sensor = Sensor(URL(string: "localhost")!)
+    
+    var cancellables: Set<AnyCancellable>!
+    
+    override func setUp() {
+        cancellables = []
+    }
+    
+    func testPlayback() {
+        
+        let mockSensor = MockSensor()
+        
+        let senseExp = expectation(description: "Sensor Reading")
+        
+        mockSensor.$currentDelta
+            .dropFirst()
+            .sink(receiveCompletion: { completion in
+                senseExp.fulfill()
+            }, receiveValue: { change in
+                print(change)
+            })
+            .store(in: &cancellables)
+        
+        mockSensor.$currentCluster
+            .dropFirst()
+            .sink { cluster in
+            cluster.printGrid()
+        }
+        .store(in: &cancellables)
+        
+        mockSensor.monitorData()
+        
+        waitForExpectations(timeout: 10, handler: nil)
+        
+//        twoPassData.enumerated().forEach { index, data in
+//            let pixels = sensor.findRelevantPixels(data)
+//
+//            print("Frame \(index)")
+//            if let cluster = sensor.clusterPixels(data).filter({ $0.size >= sensor.minClusterSize }).largest() {
+//                cluster.printGrid()
+//            } else {
+//                pixels.printGrid()
+//            }
+//        }
+    }
+    
+    func testFindPixels() {
+        let payload = SensorPayload(sensor: "AMG8833", data: "21.0,20.0,21.0,21.0,21.0,21.5,21.0,22.0,19.5,20.0,20.0,19.5,20.0,20.0,20.5,21.0,19.5,20.0,20.0,21.5,20.0,20.0,20.5,21.0,20.0,20.0,21.0,22.5,21.5,20.5,21.0,21.5,20.0,21.0,24.0,25.0,26.0,23.0,21.0,21.5,20.5,21.0,26.0,26.0,26.5,26.0,23.0,22.0,21.0,20.0,25.0,26.5,26.0,26.0,22.5,21.5,20.0,20.0,21.0,22.0,23.0,22.5,21.0,21.0")
+        
+        let pixels = sensor.findRelevantPixels(payload)
+        
+        pixels.printGrid()
+        
+
+    }
+    
+    func testClustering() {
+        let payload = SensorPayload(sensor: "AMG8833", data: "21.0,20.0,21.0,21.0,21.0,21.5,21.0,22.0,19.5,20.0,20.0,19.5,20.0,20.0,20.5,21.0,19.5,20.0,20.0,21.5,20.0,20.0,20.5,21.0,20.0,20.0,21.0,22.5,21.5,20.5,21.0,21.5,20.0,21.0,24.0,25.0,26.0,23.0,21.0,21.5,20.5,21.0,26.0,26.0,26.5,26.0,23.0,22.0,21.0,20.0,25.0,26.5,26.0,26.0,22.5,21.5,20.0,20.0,21.0,22.0,23.0,22.5,21.0,21.0")
+        
+        let clusters = sensor.clusterPixels(payload)
+        
+        clusters.forEach { cluster in
+            cluster.printGrid()
+        }
+        
+        
+    }
 
     func testBoundingBox() {
         let payload = SensorPayload(sensor: "AMG8833", data: "24.00,25.25,25.75,26.75,25.00,27.75,27.50,27.25,22.50,23.25,24.75,25.00,25.00,27.75,27.25,27.00,22.00,21.75,22.50,23.25,25.50,27.75,27.25,27.00,21.75,21.75,22.00,22.25,24.25,27.50,27.50,27.00,21.25,21.25,22.50,23.25,24.25,25.50,26.50,26.75,21.50,21.75,22.25,23.50,23.25,22.50,26.00,27.00,22.75,21.50,23.25,23.50,21.50,21.75,23.25,25.50,22.00,24.00,24.25,22.00,21.75,21.50,21.25,22.25")
@@ -38,8 +105,8 @@ class ClusterTests: XCTestCase {
 //        print(box.maxX - box.minX)
 //        print(box.maxY - box.minY)
         
-//        XCTAssertEqual(center?.x, 7)
-//        XCTAssertEqual(center?.y, 4)
+        XCTAssertEqual(center.x, 5)
+        XCTAssertEqual(center.y, 5)
 //        XCTAssertEqual(box.maxX, 7)
 //        XCTAssertEqual(box.maxY, 5)
         
