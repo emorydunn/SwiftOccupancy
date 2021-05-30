68df789cef5062b5642e16143639ba446b4261a2
Emory Dunn <edunn@emorydunn.com>
2021-05-30 13:21:22 -0700

    Update tests

--- Tests/RoomOccupancyKitTests/ThermalViewerTests.swift
+++ Tests/RoomOccupancyKitTests/ThermalViewerTests.swift
@@ -56,57 +56,65 @@ class ClusterTests: XCTestCase {
     }
     
     func testFindPixels() {
-        let payload = SensorPayload(sensor: "AMG8833", data: "21.0,20.0,21.0,21.0,21.0,21.5,21.0,22.0,19.5,20.0,20.0,19.5,20.0,20.0,20.5,21.0,19.5,20.0,20.0,21.5,20.0,20.0,20.5,21.0,20.0,20.0,21.0,22.5,21.5,20.5,21.0,21.5,20.0,21.0,24.0,25.0,26.0,23.0,21.0,21.5,20.5,21.0,26.0,26.0,26.5,26.0,23.0,22.0,21.0,20.0,25.0,26.5,26.0,26.0,22.5,21.5,20.0,20.0,21.0,22.0,23.0,22.5,21.0,21.0")
-        
-//        let pixels = sensor.findRelevantPixels(payload)
-//
-//        pixels.printGrid()
-        
-        
+        let payload = SensorPayload(sensor: "AMG8833", data: "21.020.021.021.021.021.521.022.019.520.020.019.520.020.020.521.019.520.020.021.520.020.020.521.020.020.021.022.521.520.521.021.520.021.024.025.026.023.021.021.520.521.026.026.026.526.023.022.021.020.025.026.526.026.022.521.520.020.021.022.023.022.521.021.0")!
+        Just(payload.pixels)
+            .findRelevantPixels(averageTemperature: 21, deltaThreshold: 2)
+            .sink { pixels in
+                pixels.printGrid()
+                XCTAssertEqual(pixels.count, 17)
+            }
+            .store(in: &cancellables)
+ 
     }
     
     func testClustering() {
-        let payload = SensorPayload(sensor: "AMG8833", data: "21.0,20.0,21.0,21.0,21.0,21.5,21.0,22.0,19.5,20.0,20.0,19.5,20.0,20.0,20.5,21.0,19.5,20.0,20.0,21.5,20.0,20.0,20.5,21.0,20.0,20.0,21.0,22.5,21.5,20.5,21.0,21.5,20.0,21.0,24.0,25.0,26.0,23.0,21.0,21.5,20.5,21.0,26.0,26.0,26.5,26.0,23.0,22.0,21.0,20.0,25.0,26.5,26.0,26.0,22.5,21.5,20.0,20.0,21.0,22.0,23.0,22.5,21.0,21.0")
-        
-//        let clusters = sensor.clusterPixels(payload)
-//
-//        clusters.forEach { cluster in
-//            cluster.printGrid()
-//        }
-        
-        
+        let payload = SensorPayload(sensor: "AMG8833", data: "22.021.022.024.021.022.023.024.020.022.026.026.025.021.021.023.021.024.026.025.025.023.021.022.022.025.027.026.025.024.022.022.020.020.022.024.023.020.021.021.021.020.021.020.021.020.021.021.019.020.020.020.020.020.021.021.020.020.021.020.020.020.020.021.0")!
+        Just(payload.pixels)
+            .findRelevantPixels(averageTemperature: 21, deltaThreshold: 2)
+            .clusterHotestPixels()
+            .sink { cluster in
+                cluster?.printGrid()
+                XCTAssertNotNil(cluster)
+                
+                XCTAssertEqual(cluster?.center.x, 4)
+                XCTAssertEqual(cluster?.center.y, 3)
+            }
+            .store(in: &cancellables)
+
     }
     
     func testBoundingBox() {
-        let payload = SensorPayload(sensor: "AMG8833", data: "24.00,25.25,25.75,26.75,25.00,27.75,27.50,27.25,22.50,23.25,24.75,25.00,25.00,27.75,27.25,27.00,22.00,21.75,22.50,23.25,25.50,27.75,27.25,27.00,21.75,21.75,22.00,22.25,24.25,27.50,27.50,27.00,21.25,21.25,22.50,23.25,24.25,25.50,26.50,26.75,21.50,21.75,22.25,23.50,23.25,22.50,26.00,27.00,22.75,21.50,23.25,23.50,21.50,21.75,23.25,25.50,22.00,24.00,24.25,22.00,21.75,21.50,21.25,22.25")
-        
-//        let cluster = sensor.clusterPixels(payload).largest()!
-//
-//        let box = cluster.boundingBox()
-//
-//        XCTAssertEqual(box.minX, 6)
-//        XCTAssertEqual(box.minY, 1)
-//        XCTAssertEqual(box.maxX, 8)
-//        XCTAssertEqual(box.maxY, 6)
+        let payload = SensorPayload(sensor: "AMG8833", data: "21.020.021.021.021.021.521.022.019.520.020.019.520.020.020.521.019.520.020.021.520.020.020.521.020.020.021.022.521.520.521.021.520.021.024.025.026.023.021.021.520.521.026.026.026.526.023.022.021.020.025.026.526.026.022.521.520.020.021.022.023.022.521.021.0")!
+        Just(payload.pixels)
+            .findRelevantPixels(averageTemperature: 21, deltaThreshold: 2)
+            .clusterHotestPixels()
+            .compactMap { $0 }
+            .sink { cluster in
+                let bb = cluster.boundingBox()
+                
+                XCTAssertEqual(bb.maxX, 7)
+                XCTAssertEqual(bb.maxY, 8)
+                XCTAssertEqual(bb.minX, 3)
+                XCTAssertEqual(bb.minY, 4)
+
+            }
+            .store(in: &cancellables)
         
     }
     
     func testCenter() {
-        let payload = SensorPayload(sensor: "AMG8833", data: "20.75,20.75,21.25,21.5,21.0,23.25,22.0,22.0,21.0,21.75,22.0,22.75,22.75,23.75,22.0,22.25,21.25,21.75,24.75,26.5,26.75,26.25,23.5,23.0,21.0,22.25,25.75,26.25,26.25,25.75,23.75,22.75,21.75,23.75,26.25,26.5,25.75,26.25,24.0,22.0,21.75,21.75,25.25,25.75,25.75,27.25,24.0,23.25,20.75,21.25,22.0,23.25,23.0,22.5,22.5,23.75,20.75,20.75,20.5,21.75,21.0,22.0,21.0,22.5")
+        let payload = SensorPayload(sensor: "AMG8833", data: "21.020.021.021.021.021.521.022.019.520.020.019.520.020.020.521.019.520.020.021.520.020.020.521.020.020.021.022.521.520.521.021.520.021.024.025.026.023.021.021.520.521.026.026.026.526.023.022.021.020.025.026.526.026.022.521.520.020.021.022.023.022.521.021.0")!
+        Just(payload.pixels)
+            .findRelevantPixels(averageTemperature: 21, deltaThreshold: 2)
+            .clusterHotestPixels()
+            .compactMap { $0 }
+            .sink { cluster in
+                XCTAssertEqual(cluster.center.x, 5)
+                XCTAssertEqual(cluster.center.y, 6)
+                
+            }
+            .store(in: &cancellables)
         
-//        let cluster = sensor.clusterPixels(payload).largest()!
-//        
-//        //        let box = cluster.boundingBox()
-//        let center = cluster.center
-//        
-//        
-//        //        print(box.maxX - box.minX)
-//        //        print(box.maxY - box.minY)
-//        
-//        XCTAssertEqual(center.x, 5)
-//        XCTAssertEqual(center.y, 5)
-//        //        XCTAssertEqual(box.maxX, 7)
-        //        XCTAssertEqual(box.maxY, 5)
         
     }
     
