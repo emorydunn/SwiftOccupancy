ea4ad8cf09c0bca3d81f376d49f762be101f10fd
Emory Dunn <emory@emorydunn.com>
2021-12-31 16:04:36 -0800

    Reset the buffer

--- Sources/RoomOccupancyKit/Model/BufferWrapper.swift
+++ Sources/RoomOccupancyKit/Model/BufferWrapper.swift
@@ -10,23 +10,23 @@ import Foundation
 @propertyWrapper
 struct Buffer<Value> {
     var bufferedValue: Value?
-    let count: Int
+    let bufferSize: Int
     
     var buffer: Int
     
-    init(count: Int) {
-        self.count = count
-        self.buffer = count
+    init(bufferSize: Int) {
+        self.bufferSize = bufferSize
+        self.buffer = bufferSize
     }
-    
+
     var wrappedValue: Value? {
         get { bufferedValue }
         
         set {
-
-            // If the new value isn't nil, set it
+            // If the new value isn't nil, store and reset the buffer
             if let newValue = newValue {
                 self.bufferedValue = newValue
+                buffer = bufferSize
             } else if buffer == 0 {
                 // If the buffer is at 0, clear the value
                 self.bufferedValue = nil
